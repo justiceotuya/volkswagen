079076e21f3978984187c91cf9216263cff5b390
Justice Otuya <jotuya2@gmail.com>
2018-08-23 10:11:31 +0100

    restructured the whole page

--- script/index.js
+++ script/index.js
@@ -1,22 +1,61 @@
-let searchIcon = document.querySelector('#search-icon');
-let searchGroup = document.querySelector('#search-group');
+const nextButton = $('.next');
+const prevButton = $('.prev');
+const volkswagenDiv = $('#volkswagen');
+const volvoDiv = $('#volvo');
 
-let toggle = () => {
-  if (searchGroup.className === 'hide') {
-    searchGroup.className = 'show';
-    console.log('element shown');
-  } else {
-    searchGroup.className = 'hide';
-    console.log('element hidden');
+//scroll the pages with the keyboard
+$(document).keydown(function() {
+  if (event.which === 39) {
+    scrollFoward();
+  } else if (event.which === 37) {
+    scrollBack();
+  }
+});
+
+// scrolling the pages with button
+nextButton.on('click', event => {
+  scrollFoward();
+});
+
+prevButton.click(event => {
+  scrollBack();
+});
+
+//helper functions
+const scrollFoward = () => {
+  event.preventDefault();
+  if (volkswagenDiv.hasClass('show')) {
+    removeVolksDiv();
+    showVolvoDiv();
+  } else if (volkswagenDiv.hasClass('hide')) {
+    removeVolvoDiv();
+    showVolksDiv();
   }
 };
 
-$('.image-scroll').slick({
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  cssEase: 'ease',
-  fade: true
-});
+const scrollBack = () => {
+  event.preventDefault();
+  if (volkswagenDiv.hasClass('show')) {
+    removeVolksDiv();
+    showVolvoDiv();
+  } else if (volkswagenDiv.hasClass('hide')) {
+    removeVolvoDiv();
+    showVolksDiv();
+  }
+};
 
-Vibrant.from('../images/volkswagen.jpg').getPalette(function(err, palette) {});
+const removeVolksDiv = () => {
+  volkswagenDiv.removeClass('show').addClass('hide');
+};
+
+const showVolksDiv = () => {
+  volkswagenDiv.removeClass('hide').addClass('show');
+};
+
+const removeVolvoDiv = () => {
+  volvoDiv.removeClass('show').addClass('hide');
+};
+
+const showVolvoDiv = () => {
+  volvoDiv.removeClass('hide').addClass('show');
+};
